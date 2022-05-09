b53b47c5cc9f58a88008bfce4ecc146bc6f89ca0
Knita <ismoialex@gmail.com>
2022-05-09 11:55:56 -0400

    Frontend mentor e-commerce product main challenge finished

---
+++ README.md
@@ -0,0 +1,80 @@
+# Frontend Mentor - E-commerce product page solution
+
+This is a solution to the [E-commerce product page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ecommerce-product-page-UPsZ9MJp6). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Open a lightbox gallery by clicking on the large product image
+- Switch the large product image by clicking on the small thumbnail images
+- Add items to the cart
+- View the cart and remove items from it
+
+### Screenshot
+
+![](./images/screenshot.jpeg)
+
+### Links
+
+- Solution URL: [](https://github.com/knitaxd/knitaxd.github.io)
+- Live Site URL: [](https://knitaxd.github.io/)
+
+## My process
+
+Was my first time using the mobile-first workflow and i really love it. The challenge was so hard but finally i solved it, i had many questions and that help me a lot to learn new things. I know some things are not doing on the best way but this challenge doesn't finish here for me, my plan is to do better versions of this. 
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- Vanilla JavaScript
+
+### What I learned
+
+I take some recomendations on my last challenge, one of these recomendations was the use of semantic html, and a i did it, maybe is not the best, but i promise i'm going to do it better in the other versions and challenges.
+
+Taking just couple of lines to talk about what i am proud is not fair because CSS and JavaScript are too hard, so the hole code (especially the cart functions) makes me feel so proud.
+
+### Continued development
+
+I want to improve the semantic html, furthermore i want to get use to add more grid to have facilites with the responsive web desing. About JavaScript i need to continue learning a lot on event listeners, POO and everything!
+
+### Useful resources
+
+- [W3 Schools](https://www.w3schools.com/howto/howto_js_sidenav.asp) - This helped me a lot to do the side/nav bar responsive.
+- [W3 Schools](https://www.w3schools.com/howto/howto_js_slideshow.asp) - This helped me a lot to do the slideshow and the lightbox too.
+
+- [Udemy - Juan de la Torre](https://www.udemy.com/course/javascript-moderno-guia-definitiva-construye-10-proyectos/learn/lecture/21852556?start=0#overview) - This course helped me with the cart functions.
+
+## Author
+
+- Frontend Mentor - [@knitaxd](https://www.frontendmentor.io/profile/knitaxd)
+- Twitter - [@knitaxd](hhttps://twitter.com/knitaxd)
+- GitHub - [@knitaxd](hhttps://github.com/knitaxd)
+
+## Acknowledgments
+
+I was helped a lot with the Juan de la Torre course to do the cart functions, them i use the W3 Schools page to learn about how to do some things like the slideshow. I would have liked to use more Grid in this project.
--- app.js
+++ app.js
@@ -2,164 +2,161 @@
 const navDeploy = document.getElementById("nav__deploy");
 const navBackground = document.querySelector(".nav__background");
 
-const quantityBox = document.querySelector('.main__info--quantitybox');
-const plusIcon = document.getElementById('plus__icon');
-const minusIcon = document.getElementById('minus__icon');
-const quantityItem = document.getElementById('quantity__item--quantity');
+const quantityBox = document.querySelector(".main__info--quantitybox");
+const plusIcon = document.getElementById("plus__icon");
+const minusIcon = document.getElementById("minus__icon");
+const quantityItem = document.getElementById("quantity__item--quantity");
 
-let cart = document.getElementById('cart__box');
-const cartContent = document.querySelector('#cart__content tbody');
-const checkoutCart = document.getElementById('checkout__cart');
-const addCartButton = document.getElementById('main__info--button');
-const sneakerPrice = document.querySelector('.main__info--pricebox h2');
-const sneakerImage = document.querySelector('.product__img');
-const sneakerName = document.querySelector('.main__info--sneakerbox h1');
-const sneakerIdentify = document.querySelector('#main__info--button').getAttribute('data-id')
+let cart = document.getElementById("cart__box");
+const cartContent = document.querySelector("#cart__content tbody");
+const checkoutCart = document.getElementById("checkout__cart");
+const addCartButton = document.getElementById("main__info--button");
+const sneakerPrice = document.querySelector(".main__info--pricebox h2");
+const sneakerImage = document.querySelector(".product__img");
+const sneakerName = document.querySelector(".main__info--sneakerbox h1");
+const sneakerIdentify = document
+    .querySelector("#main__info--button")
+    .getAttribute("data-id");
 let cartItems = [];
+let totalSneakers = 0;
 
 // Slides functions
-let indexValue = 1
-showImg(indexValue)
+let indexValue = 1;
+showImg(indexValue);
 
-    // Next and previous controls for img
-function sideSlide(e){
-    showImg(indexValue +=e)
+// Next and previous controls for img
+function sideSlide(e) {
+    showImg((indexValue += e));
 }
 
-    // Thumbnail control function
-function currentSlide(e){
-    showImg(indexValue = e)
+// Thumbnail control function
+function currentSlide(e) {
+    showImg((indexValue = e));
 }
 
-    // Shows the selected img in indexValue
-function showImg(e){
-    let img = document.querySelectorAll('.product__img')
-    let thumb = document.querySelectorAll('.thumbnail__img')
-    if(e > img.length) {
-        indexValue = 1
-    } else if(e < 1){
-        indexValue = img.length
+// Shows the selected img in indexValue
+function showImg(e) {
+    let img = document.querySelectorAll(".product__img");
+    let thumb = document.querySelectorAll(".thumbnail__img");
+    if (e > img.length) {
+        indexValue = 1;
+    } else if (e < 1) {
+        indexValue = img.length;
     }
 
     for (let i = 0; i < img.length; i++) {
-        img[i].style.display = 'none';
+        img[i].style.display = "none";
     }
 
     for (i = 0; i < thumb.length; i++) {
         thumb[i].className = thumb[i].className.replace(" active__thumb", "");
     }
 
-    img[indexValue - 1].style.display = 'block';
-    thumb[indexValue - 1].className += " active__thumb"
+    img[indexValue - 1].style.display = "block";
+    thumb[indexValue - 1].className += " active__thumb";
 }
 
 // Lightbox functions
-let lightboxBg = document.querySelector('.slide__lightbox--bg')
-let indexLightbox = 1
-showLightbox(indexLightbox)
+let lightboxBg = document.querySelector(".slide__lightbox--bg");
+let indexLightbox = 1;
+showLightbox(indexLightbox);
 
-    //Next and previous controls lightbox
-function sideLightbox(n){
-    showLightbox(indexLightbox += n)
+//Next and previous controls lightbox
+function sideLightbox(n) {
+    showLightbox((indexLightbox += n));
 }
 
-    // Thumbnail control function
-function currentLightbox(n){
-    showLightbox(indexLightbox = n)
+// Thumbnail control function
+function currentLightbox(n) {
+    showLightbox((indexLightbox = n));
 }
 
-
-    // Display img 
-function showLightbox(n){
-    let img = document.querySelectorAll('.lightbox__img')
-    let thumb = document.querySelectorAll('.thumbnail__lb')
-    if (n > img.length){
-        indexLightbox = 1
-    } else if(n < 1){
-        indexLightbox = img.length
+// Display img
+function showLightbox(n) {
+    let img = document.querySelectorAll(".lightbox__img");
+    let thumb = document.querySelectorAll(".thumbnail__lb");
+    if (n > img.length) {
+        indexLightbox = 1;
+    } else if (n < 1) {
+        indexLightbox = img.length;
     }
 
     for (let i = 0; i < img.length; i++) {
-        img[i].style.display = 'none';
+        img[i].style.display = "none";
     }
 
     for (let i = 0; i < thumb.length; i++) {
-        thumb[i].className = thumb[i].className.replace(" active__thumb", "")
+        thumb[i].className = thumb[i].className.replace(" active__thumb", "");
     }
 
     img[indexLightbox - 1].style.display = "block";
-    thumb[indexLightbox - 1].className += " active__thumb"
+    thumb[indexLightbox - 1].className += " active__thumb";
 }
 
-    // Open and close lightbox
-function openLightbox (){
-    lightboxBg.style.display = 'block'
-    // let img = document.querySelectorAll('.product__img')
-    // if(lightboxBg.style.display = 'block'){
-    //     let lightboxImg = document.querySelector('.slide__lightbox')
-    //     for (let i = 0; i < img.length; i++) {
-    //         lightboxImg.appendChild(img[i])
-    //     }
-    // }
+// Open and close lightbox
+function openLightbox() {
+    lightboxBg.style.display = "block";
 }
 
-    function closeLightbox(){
-    lightboxBg.style.display = 'none'
+function closeLightbox() {
+    lightboxBg.style.display = "none";
 }
-
-
 
 // quantity functions
 let start = 1;
-function aumentar(){
-    quantityItem.value = ++start
-    if(quantityItem.value <= 1){
-        quantityItem.value = 1
+function aumentar() {
+    quantityItem.value = ++start;
+    if (quantityItem.value <= 1) {
+        quantityItem.value = 1;
     }
 }
 
-function disminuir(){
-    quantityItem.value = --start
-    if(quantityItem.value <= 1){
-        quantityItem.value = 1
+function disminuir() {
+    quantityItem.value = --start;
+    if (quantityItem.value <= 1) {
+        quantityItem.value = 1;
     }
 }
 
 // nav functions
-
 const openNav = () => {
-    navDeploy.style.width = "67%"
-    navBackground.style.width = "33%"
- }
+    navDeploy.style.width = "67%";
+    navBackground.style.width = "33%";
+};
 const closeNav = () => {
-    navDeploy.style.width = "0"
-    navBackground.style.width = "0"
-}
+    navDeploy.style.width = "0";
+    navBackground.style.width = "0";
+};
 
-navBackground.addEventListener('click', closeNav)
-
+navBackground.addEventListener("click", closeNav);
 
 // cart functions
-
-function openCart (){
-    if(cart.style.height === '0vh'){
-        cart.style.height = '30vh'
+function openCart() {
+    if (cart.style.height === "0vh") {
+        cart.style.height = "30vh";
     } else {
-        cart.style.height = '0vh'
+        cart.style.height = "0vh";
     }
 }
 
 loadEventListeners();
-function loadEventListeners(){
+function loadEventListeners() {
     // Add sneakers pressing the button
-    addCartButton.addEventListener('click', addSneaker)
+    addCartButton.addEventListener("click", addSneaker);
+    addCartButton.addEventListener("click", cartQuantity);
 
     // Delete sneakers in the cart
-    cart.addEventListener('click', deleteSneaker)
+    cart.addEventListener("click", deleteSneaker);
 }
 
-function addSneaker(){
+// Count the over items in the cart
+function cartQuantity() {
+    let overNumber = document.querySelector("#number__items");
+    totalSneakers = totalSneakers + parseInt(quantityItem.value);
+    overNumber.textContent = `${totalSneakers}`;
+}
 
+function addSneaker() {
     // object with sneaker info
     const sneakerInfo = {
         img: sneakerImage.src,
@@ -167,69 +164,66 @@ function addSneaker(){
         price: sneakerPrice.textContent,
         quantity: parseInt(quantityItem.value),
         finalPrice: sneakerTotal(),
-        id: sneakerIdentify
-    }
-
+        id: sneakerIdentify,
+    };
 
     // Check if an sneaker is in the cart
-    const exist = cartItems.some( sneaker => sneaker.id === sneakerInfo.id)
-    if(exist){
+    const exist = cartItems.some((sneaker) => sneaker.id === sneakerInfo.id);
+    if (exist) {
         // Update the quantity if the sneaker already exist in the cart
-        const sneakers = cartItems.map( sneaker => {
-            if (sneaker.id === sneakerInfo.id){
-                sneaker.quantity = sneaker.quantity + parseInt(quantityItem.value)
-                
-                overQuantity()
-                function overQuantity(){
-                    let overNumber = document.querySelector("#number__items")
-                    overNumber.innerHTML = `${sneaker.quantity}`
-                }
-                sneaker.finalPrice = parseInt(sneakerPrice.textContent.substring(1)) * sneaker.quantity // Calculate the final price
+        const sneakers = cartItems.map((sneaker) => {
+            if (sneaker.id === sneakerInfo.id) {
+                sneaker.quantity = sneaker.quantity + parseInt(quantityItem.value);
+                sneaker.finalPrice =
+                    parseInt(sneakerPrice.textContent.substring(1)) * sneaker.quantity; // Calculate the final price
                 return sneaker; // return updated object
-            } else{
+            } else {
                 return sneaker; // return not duplicated objects
             }
-        } )
+        });
 
-        cartItems = [...sneakers]
-    }else{
-         // add items to cart
-        cartItems = [...cartItems, sneakerInfo]
+        cartItems = [...sneakers];
+    } else {
+        // add items to cart
+        cartItems = [...cartItems, sneakerInfo];
     }
-    
-    cartHTML()
+
+    cartHTML();
 
     // total price of quantity sneakers
-    function sneakerTotal(){
-        return totalPrice = parseInt(sneakerPrice.textContent.substring(1)) * quantityItem.value
+    function sneakerTotal() {
+        return (totalPrice =
+            parseInt(sneakerPrice.textContent.substring(1)) * quantityItem.value);
     }
 }
 
 // Delete sneaker in the cart
-function deleteSneaker(e){
-    if(e.target.classList.contains('delete__item--button')){
-        const sneakerId = e.target.getAttribute('data-id')
-    
-        // Delete the sneaker of the cart 
-        cartItems = cartItems.filter ( sneaker => sneaker.id !== sneakerId)
-        
-        cartHTML(); // Iterate over the cart to refresh the HTML    
+function deleteSneaker(e) {
+    let overNumber = document.querySelector("#number__items");
+    if (e.target.classList.contains("delete__item--button")) {
+        const sneakerId = e.target.getAttribute("data-id");
+
+        // Delete the sneaker of the cart
+        cartItems = cartItems.filter((sneaker) => sneaker.id !== sneakerId);
+
+        // Refreg th over sneakers quantity
+        overNumber.textContent = 0;
+        totalSneakers = 0;
+
+        cartHTML(); // Iterate over the cart to refresh the HTML
     }
 }
 
+// insert html data in cart
+function cartHTML() {
+    // clean HTML
+    limpiarHTML();
 
-    // insert html data in cart
-    function cartHTML(){
-
-
-        // clean HTML
-        limpiarHTML()
-
-        // loop the cart and generate the HTML
-        cartItems.forEach( sneaker => {
-            const {img, title, price, quantity, finalPrice, id} = sneaker
-            const row = document.createElement('tr');
-            row.innerHTML = `
+    // loop the cart and generate the HTML
+    cartItems.forEach((sneaker) => {
+        const { img, title, price, quantity, finalPrice, id } = sneaker;
+        const row = document.createElement("tr");
+        row.innerHTML = `
                 <td>
                     <img src="${img}" class="img__cart">
                 </td>
@@ -242,38 +236,24 @@ function deleteSneaker(e){
                 </td>
             `;
 
-            // Fill the tbody with items
-            checkoutCart.style.display = 'flex'
-            cartContent.appendChild(row)
-
-            
-
-        })
-        // Checkout if cart have sneakers
-        if (cartContent.children.length === 0) {
-            const row = document.createElement('tr')
-            row.setAttribute('id', 'cart__empty--message')
-            row.className = 'center'
-            checkoutCart.style.display = 'none'
-            row.innerHTML = '<td>Your cart is empty.</td>'
-            cartContent.appendChild(row)
-        }       
-    }
-
-
-    // Cleaning code before pushin in the tbody
-function limpiarHTML(){
-    while (cartContent.firstChild) {
-        cartContent.removeChild(cartContent.firstChild)
+        // Fill the tbody with items
+        checkoutCart.style.display = "flex";
+        cartContent.appendChild(row);
+    });
+    // Checkout if cart have sneakers
+    if (cartContent.children.length === 0) {
+        const row = document.createElement("tr");
+        row.setAttribute("id", "cart__empty--message");
+        row.className = "center";
+        checkoutCart.style.display = "none";
+        row.innerHTML = "<td>Your cart is empty.</td>";
+        cartContent.appendChild(row);
     }
 }
 
-
-
-
-
-
-
-
-
-
+// Cleaning code before pushin in the tbody
+function limpiarHTML() {
+    while (cartContent.firstChild) {
+        cartContent.removeChild(cartContent.firstChild);
+    }
+}
